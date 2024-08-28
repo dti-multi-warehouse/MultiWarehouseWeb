7921a7e3c624818e3091f642821ac5304735a50c
Gerry Sheva <gerry.sheva@proton.me>
2024-08-29 03:33:38 +0700

    fix: increase gap between UserInfo element

--- components/DashboardHeader/UserInfo.tsx
+++ components/DashboardHeader/UserInfo.tsx
@@ -4,7 +4,7 @@ import {CircleChevronDown} from "lucide-react";
 
 const UserInfo: FC = () => {
     return (
-        <div className={"flex gap-2 items-center"}>
+        <div className={"flex gap-4 items-center"}>
             <Avatar.Root className={"w-12 h-12 inline-flex border border-white rounded-full bg-red-500 text-white font-semibold"}>
                 <Avatar.AvatarFallback className={"w-full h-full flex items-center justify-center"}>R</Avatar.AvatarFallback>
             </Avatar.Root>
