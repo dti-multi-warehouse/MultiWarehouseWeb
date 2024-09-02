e37f44f886e8b5a8306e1783a98a5979ccc97e89
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-09-03 00:51:14 +0700

    feat: WIP retrieve product details for update

---
+++ app/dashboard/products/(crud)/edit/[id]/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import {FC} from "react";
+import ProductForm from "@/app/dashboard/products/(crud)/ProductForm";
+import {FileWithPreview, ProductData} from "@/app/dashboard/products/types";
+import axios from "axios";
+import useProductDetails from "@/hooks/useProductDetails";
+
+interface Props {
+    params: {
+        id: number;
+    }
+}
+
+const EditProductPage: FC<Props> = ({params}) => {
+    const { data, isLoading, error } = useProductDetails(params.id)
+    console.log(data)
+
+    const handleSubmit = (values: ProductData, images: FileWithPreview[]) => {
+        const formData = new FormData()
+        formData.append("product", new Blob([JSON.stringify(values)], {type: 'application/json'}))
+        images.forEach(image => formData.append("images", image))
+        axios.post('http://localhost:8080/api/v1/product', formData)
+    }
+    return <main className={"p-8"}>
+        <h1 className={"text-3xl font-semibold"}>Edit product</h1>
+        <ProductForm handleSubmit={handleSubmit}/>
+    </main>
+}
+
+export default EditProductPage;
