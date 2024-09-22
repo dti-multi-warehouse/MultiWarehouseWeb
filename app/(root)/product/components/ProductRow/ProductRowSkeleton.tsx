e83e527665b3fef7c4628307e3cd0c422a2c0781
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-09-23 05:17:41 +0700

    feat: implement skeleton for product card

---
+++ app/(root)/product/components/ProductRow/ProductRowSkeleton.tsx
@@ -0,0 +1,19 @@
+import {FC} from "react";
+import {clsx} from "clsx";
+import ProductCardSkeleton from "@/components/ProductCard/ProductCardSkeleton";
+
+
+const ProductRowSkeleton: FC = () => {
+    const array = Array.from({ length: 20 }, (_, i) => i);
+
+    return (
+        <div className={'col-span-4 overflow-visible'}>
+            <div
+                className={clsx('grid grid-cols-2 gap-y-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 overflow-visible no-scrollbar')}>
+                {array.map( i => <ProductCardSkeleton key={i} />)}
+            </div>
+        </div>
+    )
+}
+
+export default ProductRowSkeleton
--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,26 +1,22 @@
 'use client'
 import {useSearchParams} from "next/navigation";
 import useProducts from "@/hooks/useProducts";
-import ProductRow from "@/components/ProductRow";
 import ProductPagination from "@/app/(root)/product/components/ProductPagination";
-import {Loader} from "lucide-react";
 import ProductFilter from "@/app/(root)/product/components/ProductFilter";
+import ProductRowSkeleton from "@/app/(root)/product/components/ProductRow/ProductRowSkeleton";
+import ProductRow from "@/app/(root)/product/components/ProductRow";
 
 
 const ProductPage = () => {
     const params = useSearchParams()
     const { data, isLoading, error } = useProducts(params)
 
-    if (isLoading || !data) {
-        return <Loader />
-    }
-
     return <main className={""}>
         <div className={"grid grid-cols-5 gap-4"}>
             <ProductFilter />
-            <ProductRow isRow={false} hits={data.hits} />
+            {isLoading || !data ? <ProductRowSkeleton /> : <ProductRow isRow={false} hits={data.hits} />}
         </div>
-        <ProductPagination currentPage={data.page} totalPages={data.totalPage} params={params}/>
+        {data && <ProductPagination currentPage={data.page} totalPages={data.totalPage} params={params}/>}
     </main>
 }
 
