034e8a5c3424de7c8b09971921fa50badca6075e
N Rizki Amalia <rizkiamel9@gmail.com>
2024-10-06 18:39:41 +0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
     typescript: {
       ignoreBuildErrors: true,
     },
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.resolve(__dirname, './'); // or './' if you're not using 'src'
+      return config;
+    },
   };
   
   export default nextConfig;
