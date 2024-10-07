9f7f1e8ab3d8b338537fed221d3fe4dd8062a1fc
N Rizki Amalia <rizkiamel9@gmail.com>
2024-10-07 21:01:50 +0700

    try fix backend link

--- constants/url.ts
+++ constants/url.ts
@@ -1,6 +1,6 @@
 export const config = {
-    BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    API_VER: 'api/v1/',
+    BASE_URL: '/',
+    API_VER: '/api/v1/',
     endpoints: {
         product: 'product',
         category: 'category',
--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getSession } from 'next-auth/react';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL?.trim() || 'https://dev.alphamarch.shop',
+  baseURL: '/',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
