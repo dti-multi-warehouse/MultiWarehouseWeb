768c249b04e3e9e2f53c4c5ccba1bfb34ff79e97
N Rizki Amalia <rizkiamel9@gmail.com>
2024-10-08 08:11:46 +0700

    next-auth

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -83,7 +83,6 @@ export const authOptions: NextAuthOptions = {
         // secure: process.env.NODE_ENV !== 'development',
         // domain: process.env.NODE_ENV === 'development' ? 'localhost' : 'dev.alphamarch.shop',
         secure: true,
-        domain: 'dev.alphamarch.shop',
       },
     },
     csrfToken: {
