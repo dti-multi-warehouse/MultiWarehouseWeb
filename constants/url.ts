8c298c9eed66019b2b2b80dd91ea2694d8ab3c9c
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-10-12 16:54:11 +0700

    fix: fix url

--- constants/url.ts
+++ constants/url.ts
@@ -1,5 +1,5 @@
 export const config = {
-    BASE_URL: 'http://localhost:8080', //api
+    BASE_URL: 'https://be.alphamarch.shop', //api
     API_VER: '/api/v1/',
     endpoints: {
         product: 'product',
