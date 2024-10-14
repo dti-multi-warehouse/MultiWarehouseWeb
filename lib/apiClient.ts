de427059e4c84c5927f0f592d64739ce191ba8d6
N Rizki Amalia <rizkiamel9@gmail.com>
2024-10-14 09:55:43 +0700

    fix

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getSession } from 'next-auth/react';
 
 const apiClient = axios.create({
-  baseURL: 'https://dev-be.alphamarch.shop', //api
+  baseURL: 'https://be.alphamarch.shop', //api
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
